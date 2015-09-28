b5f30f775b864b9cff8965114ea95f8fc845d85e
BetterYuan <qiangqiangyxs@163.com>
2015-09-28 16:54:41 +0800

    first

---
+++ YXSGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXSGitDemo
+//
+//  Created by qianfeng on 15/9/28.
+//  Copyright © 2015年 袁肖松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXSGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXSGitDemo
+//
+//  Created by qianfeng on 15/9/28.
+//  Copyright © 2015年 袁肖松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
